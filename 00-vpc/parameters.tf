c306370bc6a694cde0b2b4c6d2dc563f2a6dcf1d
Naren <lnaren532@gmail.com>
2025-06-25 15:31:01 +0530

    terraform-vpc-

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -7,20 +7,20 @@ resource "aws_ssm_parameter" "vpc_id" {
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name  = "/${var.project}/${var.environment}/public_subnet_ids"
   type  = "StringList"
-  value = join("," , module.vpc.public_subnet_ids)  
+  value = join(",", module.vpc.public_subnet_ids)  
    # we are storing the subnet_id in ssm paramerter by using the "aws_ssm_parameter" resource 
 }
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name  = "/${var.project}/${var.environment}/private_subnet_ids"
   type  = "StringList"  # it will store like   id1 , id2   <---   ["id1" , "id2"]
-  value = join("," , module.vpc.private_subnet_ids)  
+  value = join(",", module.vpc.private_subnet_ids)  
    # we are storing the subnet_id in ssm paramerter by using the "aws_ssm_parameter" resource 
 }
 
 resource "aws_ssm_parameter" "database_subnet_ids" {
   name  = "/${var.project}/${var.environment}/database_subnet_ids"
   type  = "StringList"
-  value = join("," , module.vpc.database_subnet_ids)  
+  value = join(",", module.vpc.database_subnet_ids)  
    # we are storing the subnet_id in ssm paramerter by using the "aws_ssm_parameter" resource 
 }
