dd1579663b199aa0e61fe06b4d4229ab105579ca
Naren <lnaren532@gmail.com>
2025-06-25 16:01:53 +0530

    terraform-sg

--- 10-sg-group/main.tf
+++ 10-sg-group/main.tf
@@ -1,5 +1,5 @@
-module "frontend" {  # front end security grupt
-  
+# front end security grupt
+module "frontend" {  
   source = "../../terraform-aws-securitygroup"
   project = var.project
   description = var.frontend_sg_description
@@ -8,9 +8,8 @@ module "frontend" {  # front end security grupt
     vpc_id = local.vpc_id  #vpc_id = data.aws_ssm_parameter.vpc_id.value
 
 }
-
-module "bastion" {  # bastion/jump server security groupId
-  
+# bastion/jump server security groupId creation
+module "bastion" {  
   source = "../../terraform-aws-securitygroup"
   project = var.project
   description = var.bastion_sg_description
@@ -20,7 +19,7 @@ module "bastion" {  # bastion/jump server security groupId
 
 }
 
-resource "aws_security_group_rule" "bation_laptop_ssh" {
+resource "aws_security_group_rule" "bastion_laptop_ssh" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
