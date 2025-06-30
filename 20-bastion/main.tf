0189606a53833fe50670c4c24c9e971b5039ad72
Naren <lnaren532@gmail.com>
2025-06-30 17:41:08 +0530

    update

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "bation" {
+resource "aws_instance" "bastion" {
   ami           =local.ami_id #data.aws_ami.joindevops.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
