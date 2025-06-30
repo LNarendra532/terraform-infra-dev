26082d7f7ae898cdeea76b4d39ce2d5e1c26f845
Naren <lnaren532@gmail.com>
2025-06-30 17:42:51 +0530

    update

---
+++ 60-catalogue/data.tf
@@ -0,0 +1,38 @@
+data "aws_ami" "joindevops" {
+
+  most_recent      = true
+  owners           = ["973714476881"]
+
+  filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+  }
+
+  filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+     filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+ name  = "/${var.project}/${var.environment}/private_subnet_ids"  
+
+  # from the /00-vpc/ module parameters.tf we should access the vpc_id for the for the sg_group
+}
+
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name  = "/${var.project}/${var.environment}/catalogue_sg_id"
+
+}
+
+data "aws_ssm_parameter" "vpc_id" {
+  name  = "/${var.project}/${var.environment}/vpc_id"
+
+}
---
+++ 60-catalogue/main.tf
@@ -0,0 +1,63 @@
+#Creating Catalogue instances 
+resource "aws_instance" "catalogue" {
+  ami           =local.ami_id #data.aws_ami.joindevops.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  subnet_id = local.private_subnet_id
+  #iam_instance_profile = "EC2RoleToFetchSSMParams"
+
+  tags = merge(
+
+    local.common_tags, {
+      Name = "${var.project}-${var.environment}-catalogue-server" #interpolation
+    }
+  )
+    
+  }
+# target group opening on 8080 http
+resource "aws_lb_target_group" "catalogue" {
+  name        = "${var.environment}-${var.project}-catalogue"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = local.vpc_id
+
+  health_check {
+    healthy_threshold = 2
+    interval = 5
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    timeout = 2
+    unhealthy_threshold = 3
+  }
+}
+  
+
+# Null_resource - it will create any resource , it will connect to the instances and we can perform our actions.
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  
+  # Copies the bootstrap.sh file into /tmp
+  provisioner "file" {
+    source      = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.catalogue.private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/catalogue.sh",
+      "sudo sh /tmp/catalogue.sh catalogue"
+    ]
+  }
+}
+
