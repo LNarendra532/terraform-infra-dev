64df434fe51bd23f230123c743563abfd3f8d9fe
Naren <lnaren532@gmail.com>
2025-07-02 21:28:41 +0530

    update

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,12 +1,13 @@
 locals {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]# minimum two required
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)# minimum two required
     ami_id =data.aws_ami.joindevops.id
     catalogue_sg_id =data.aws_ssm_parameter.catalogue_sg_id.value
-    
-    
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
 
-     common_tags = {
+    
+    common_tags = {
     Project     = var.project
     Environment = var.environment
     Terraform   = "true"
